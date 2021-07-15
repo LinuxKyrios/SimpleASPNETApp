202d46f2e8d5145358991730929d85ab90052043
Linux Kyrios <malinowskim92@protonmail.com>
2021-07-15 09:00:08 +0200

    Modified HomeController.cs file to display new product

--- SimpleASPNETApp/Controllers/HomeController.cs
+++ SimpleASPNETApp/Controllers/HomeController.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleASPNETApp.Models;
 
 namespace SimpleASPNETApp.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public string Index()
         {
-            return View ();
+            return "Passage to example URL adress";
+
+        }
+
+        public ViewResult AutoProperty()
+        {
+            //Creating new object Product
+            Product myProduct = new Product();
+
+            //Setting value
+            myProduct.Name = "POLSpeed paper";
+
+            //Reading value
+            string productName = myProduct.Name;
+
+            return View("Result", (object)String.Format("Product name: {0}", productName));
         }
     }
 }
